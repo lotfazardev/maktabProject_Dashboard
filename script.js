148f1c4b2e10fcd0a268ce60a93ec303ac1e5b57
Mohammad H. Lotfazar <lotfazardev@users.noreply.github.com>
2020-04-07 14:30:44 +0430

    middle section

---
+++ script.js
@@ -0,0 +1,69 @@
+let canvas = document.getElementById('chart');
+let context = canvas.getContext('2d');
+let grad = context.createLinearGradient(100,200,100,0);
+grad.addColorStop(0, 'rgba(255,255,255,1)');
+grad.addColorStop(0.3, 'rgba(255,255,255,1)');
+grad.addColorStop(1, 'rgba(249,226,185,1)');
+new Chart(document.getElementById("chart"), {
+    type: 'line',
+    defaultFontFamily: Chart.defaults.global.defaultFontFamily = "'shabnam'",
+    data: {
+      labels: [0,5,10,15,20,25,30],
+      datasets: [
+          { 
+          data: [18,20,32,9,47,10,30],
+          label: "تیکت ها",
+          lineTension: 0,
+          borderColor: "#F19C1C",
+          backgroundColor: grad
+          },
+          { 
+            data: [],
+            label: "دانشنامه",
+            borderColor: "#e2e2e2"
+          },
+          { 
+            data: [],
+            label: "خبر",
+            borderColor: "#e2e2e2"
+          }
+      ]
+    },
+    options: {
+        legend: {
+            display: true,
+            fullWidth:true,
+            rtl:true,
+            position:'bottom',
+            labels:{
+                fontFamily:"shabnam",
+                fontColor:"#03000F",
+                padding:15,
+                usePointStyle:true
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: 10,
+                    min: 0,
+                    max: 50
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'تعداد'
+                }
+            }],
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'روز'
+                },
+                gridLines : {
+                    display : false
+                }
+            }]
+        }
+    }
+  });
